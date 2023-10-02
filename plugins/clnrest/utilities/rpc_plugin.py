977f85d8080026da8f02cb937c61f5a19e106cf0
Tony Aldon <tony@tonyaldon.com>
2023-10-02 14:09:25 +0200

    clnrest: fix clnrest dependency on cln-grpc plugin certificates.
    Changelog-Fixed: clnrest no longer depends on cln-grpc plugin certificates.

--- plugins/clnrest/utilities/rpc_plugin.py
+++ plugins/clnrest/utilities/rpc_plugin.py
@@ -1,9 +1,12 @@
 import os
 from pyln.client import Plugin
+from pathlib import Path
 
 plugin = Plugin(autopatch=False)
 
-plugin.add_option(name="rest-certs", default=os.getcwd(), description="Path for certificates (for https)", opt_type="string", deprecated=False)
+rest_certs = Path(os.getcwd()) / 'clnrest'
+
+plugin.add_option(name="rest-certs", default=rest_certs.as_posix(), description="Path for certificates (for https)", opt_type="string", deprecated=False)
 plugin.add_option(name="rest-protocol", default="https", description="REST server protocol", opt_type="string", deprecated=False)
 plugin.add_option(name="rest-host", default="127.0.0.1", description="REST server host", opt_type="string", deprecated=False)
 plugin.add_option(name="rest-port", default=None, description="REST server port to listen", opt_type="int", deprecated=False)
