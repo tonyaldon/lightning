5d920ffd7aa30800be7728e983e1321613cff1b4
Tony Aldon <tony@tonyaldon.com>
2023-10-02 14:02:51 +0200

    pytest: test that clnrest doesn't depend on cln-grpc plugin certificates.
    This fails currently, so next commit fix it up.
    
    Error:
    
    ```
    requests.exceptions.SSLError: HTTPSConnectionPool(host='127.0.0.1', port=40917):
    Max retries exceeded with url: /v1/list-methods
    (Caused by SSLError(SSLCertVerificationError
    (1, "[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: IP address mismatch,
    certificate is not valid for '127.0.0.1'. (_ssl.c:1007)")))
    ```

--- tests/test_clnrest.py
+++ tests/test_clnrest.py
@@ -1,6 +1,7 @@
 from ephemeral_port_reserve import reserve
 from fixtures import *  # noqa: F401,F403
-from pyln.testing.utils import wait_for
+from pyln.testing.utils import env, wait_for
+import unittest
 import requests
 from pathlib import Path
 
@@ -13,6 +14,27 @@ def test_clnrest_self_signed_certificates(node_factory):
     l1 = node_factory.get_node(options={'disable-plugin': 'cln-grpc',
                                         'rest-port': rest_port,
                                         'rest-host': rest_host})
+    wait_for(lambda: l1.daemon.is_in_log(r'plugin-clnrest.py: REST server running at ' + base_url))
+    rest_certs_default = Path(l1.rpc.listconfigs()['configs']['rest-certs']['value_str'])
+    ca_cert_path = rest_certs_default / 'ca.pem'
+    r = requests.get(base_url + '/v1/list-methods', verify=ca_cert_path)
+    assert r.status_code == 200
+
+
+@unittest.skipIf(env('RUST') != '1', 'RUST is not enabled skipping rust-dependent tests')
+def test_clnrest_does_not_depend_on_grpc_plugin_certificates(node_factory):
+    """Test that clnrest doesn't depend on `cln-grpc` plugin certificates when started with default options.
+
+    Defaults:
+    - rest-port: 127.0.0.1
+    - rest-protocol: https
+    """
+    grpc_port = str(reserve())
+    rest_port = str(reserve())
+    l1 = node_factory.get_node(options={'grpc-port': grpc_port,
+                                        'rest-port': rest_port})
+    base_url = 'https://127.0.0.1:' + rest_port
+    wait_for(lambda: l1.daemon.is_in_log(r'serving grpc on 0.0.0.0:'))
     wait_for(lambda: l1.daemon.is_in_log(r'plugin-clnrest.py: REST server running at ' + base_url))
     rest_certs_default = Path(l1.rpc.listconfigs()['configs']['rest-certs']['value_str'])
     ca_cert_path = rest_certs_default / 'ca.pem'
